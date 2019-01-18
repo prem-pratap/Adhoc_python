d1d1fceae1c6a7795c6d7857a37adfe61e34b7f7
Prem Pratap Singh <46396311+prem-pratap@users.noreply.github.com>
2019-01-18 14:03:41 +0530

    Add files via upload

---
+++ unintrupted.py
@@ -0,0 +1,34 @@
+#!usr/bin/env python
+
+import socket
+import thread
+
+ip="127.0.0.1"  #receivers ip
+port=9056  #port > 6000 are 
+#calling UDP protocol
+#if no arguements passed to socket() then by default it becomes TCP
+
+#socket.SOCK_DGRAM  ---> UDP   socket.AF_INET----> ipv4
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+#sending message to target
+s.bind((ip,port))
+clients=[]
+def sending():
+	while True:
+		accpt=raw_input()
+		s.sendto("Other user: "+accpt,(ip,9055))
+def recving():
+	while True:
+		data=s.recvfrom(1024)
+		print(data[0])
+try:
+	thread.start_new_thread(sending,())
+	thread.start_new_thread(recving,())
+except:
+	print("BUg")
+while True:
+	pass
+
+
+
